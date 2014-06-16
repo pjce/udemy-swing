164cdeee705a3a3f2d3567751ec88763b68bcc2c
Peter Evans <pjcepost@gmail.com>
2014-06-16 14:55:54 +0100

    Added borders

--- Udemy_Swing1_tut/src/TextPanel.java
+++ Udemy_Swing1_tut/src/TextPanel.java
@@ -5,18 +5,19 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class TextPanel extends JPanel {
-	
-	private JTextArea textArea;
 
+	private JTextArea textArea;
 
 	public TextPanel() {
 		textArea = new JTextArea();
-		
+
 		setLayout(new BorderLayout());
-		
+
 		add(new JScrollPane(textArea), BorderLayout.CENTER);
 	}
-	
+
+	/* **************************************************** */
+
 	public void appendText(String text) {
 		textArea.append(text);
 	}
--- Udemy_Swing1_tut/src/Toolbar.java
+++ Udemy_Swing1_tut/src/Toolbar.java
@@ -2,49 +2,55 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-
 
 public class Toolbar extends JPanel implements ActionListener {
 	private JButton helloButton;
 	private JButton goodbyeButton;
-	
+
 	private StringListener textListener;
-	
+
 	public Toolbar() {
+
+		setBorder(BorderFactory.createEtchedBorder());
+
 		helloButton = new JButton("Hello");
 		goodbyeButton = new JButton("Goodbye");
-		
+
 		helloButton.addActionListener(this);
 		goodbyeButton.addActionListener(this);
- 
+
 		setLayout(new FlowLayout(FlowLayout.LEFT));
-		
+
 		add(helloButton);
 		add(goodbyeButton);
+
 	}
-	
+
+	/* **************************************************** */
+
 	public void setStringListener(StringListener listener) {
 		this.textListener = listener;
 	}
 
+	/* **************************************************** */
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		JButton clicked = (JButton)e.getSource();
-		
-		if(clicked == helloButton) {
-			if(textListener != null) {
+		JButton clicked = (JButton) e.getSource();
+
+		if (clicked == helloButton) {
+			if (textListener != null) {
 				textListener.textEmitted("Hello\n");
 			}
-//			textPanel.appendText("Hello\n");
-		} 
-		else if (clicked == goodbyeButton) {
-//			textPanel.appendText("Goodbye\n");
-			if(textListener != null) {
+			// textPanel.appendText("Hello\n");
+		} else if (clicked == goodbyeButton) {
+			// textPanel.appendText("Goodbye\n");
+			if (textListener != null) {
 				textListener.textEmitted("Goodbye\n");
 			}
-
 
 		}
 	}
